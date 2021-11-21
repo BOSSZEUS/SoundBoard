2e20e60d33c29eb8e3cef98fc57ac7f3ef93a227
Jordan Scheftic <47509698+BOSSZEUS@users.noreply.github.com>
2021-11-20 20:36:18 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const sounds = ['Anime','directed','fat-man','fbi-open-up','epic-sax','coffin-dance','mm-whatcha-say','you-got-that']
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+
+        document.getElementById(sound).play()
+        
+
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
